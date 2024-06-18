90dc3d4787b9c61bfdeb6bfa7b286fec2bb64658
Bia <82560900+BiatrizR@users.noreply.github.com>
2024-06-18 18:08:24 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,32 @@
 
 //código para rolagem dos livros com swuiper
 var swiper = new Swiper(".books-slider", {
+  spaceBetween:10,  
+  loop: true,
+    centerSlides: true,
+    autoplay: {
+        delay:9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 5,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+        
+      },
+    },
+  });
+  
+
+//! Para a seção de fetures, copiar o swiper slider e adicionar o nome da classe onde se encontra os elementos da feturead
+  var swiper = new Swiper(".featured-slider", {
+    spaceBetween:10,
     loop: true,
     centerSlides: true,
     autoplay: {
@@ -22,3 +48,6 @@ var swiper = new Swiper(".books-slider", {
       },
     },
   });
+
+
+
